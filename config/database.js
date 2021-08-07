d509a11df326eb715782fb5e4349da9094bfc351
shaheen5 <miyashaheen1995@gmail.com>
2021-08-07 19:32:50 +0530

    feat : connect mongoDB to our app using mongoose

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const env = require('dotenv');
+env.config();
+module.exports = () => {
+    mongoose.connect(process.env.URL,
+        {
+            useNewUrlParser: true, useUnifiedTopology: true
+        }).then(() => {
+            console.log("Successfully connected to the database");
+        }).catch(err => {
+            console.log('Could not connect to the database. Exiting now...', err);
+            process.exit();
+        });
+    
+        return mongoose.connection;
+}
+
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const env = require('dotenv');
+
 env.config();
 const app = express();
+
+// Configuring the database
+const dbConnect = require('./config/database');
+dbConnect();
+
+app.get('/', (req, res) => {
+  res.json({ message: "E-Commerce Application Backend"})
+});
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
 });
